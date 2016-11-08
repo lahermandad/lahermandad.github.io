21f6bdacbe93017ac0c67da1a06a36dae7a412db
JohanCHM <hansen.mendoza@gmail.com>
2016-11-08 14:38:32 -0600

    Face y Quiz2
    Se añadieron los Share de Face de Quiz 1.
    
    Del Quiz 2 se añadieron las imágenes y se mostraron en display.

--- js/hermandadJS.js
+++ js/hermandadJS.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
     setTimeout(function() {
         $('#txt_titulo').fadeIn(900);
@@ -11,7 +10,7 @@ $(document).ready(function() {
     }, 3000);
 });
 
-/* -------------FUNCTIONS ------------
+// -------------FUNCTIONS ------------
 
 /**
  * Oculta y muestra un div con display none
@@ -21,4 +20,107 @@ $(document).ready(function() {
 function cambiar_div(origen, destino) {
     $(origen).fadeOut(500);
     $(destino).fadeIn(2500);
+}
+
+
+// -------------FACEBOOK ------------
+// TODO: Ver si es necesario algún #
+
+window.onload = function() {
+    FB.init({
+        appId: '1166672466755694',
+        xfbml: true,
+        version: 'v2.8'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/es_LA/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function shareStory() {
+    FB.ui({
+        method: 'share',
+        href: 'https://lahermandad.github.io/index_rol.html',
+        description: 'Soy una persona astuta y de gran inteligencia, capaz de cuidar con gran afición lo que amo, pero cuidado con los que se metan en mi camino.',
+        title: 'Soy como Pedro de La Hermandad. ¿Tú conoces cómo es tu personalidad?',
+        picture: 'https://lahermandad.github.io/pic/q1_pedro.jpg',
+    }, function(response) {
+        if (response && !response.error_message) {
+            $('#lb_msj').text('link posteado' + response.post_id);
+        } else {
+            $('#lb_msj').text('error ...');
+        }
+    });
+}
+
+function sharePedro() {
+    FB.ui({
+        method: 'share',
+        href: 'https://lahermandad.github.io/index_rol.html',
+        description: 'Soy una persona astuta y de gran inteligencia, capaz de cuidar con gran afición lo que amo, pero cuidado con los que se metan en mi camino.',
+        title: 'Soy como Pedro de La Hermandad. ¿Tú conoces cómo es tu personalidad?',
+        picture: 'https://lahermandad.github.io/pic/q1_pedro.jpg',
+    }, function(response) {
+        if (response && !response.error_message) {
+            $('#lb_msj').text('link posteado' + response.post_id);
+        } else {
+            $('#lb_msj').text('error ...');
+        }
+    });
+}
+
+function shareJulio() {
+    FB.ui({
+        method: 'share',
+        href: 'https://lahermandad.github.io/index_rol.html',
+        description: 'Soy una persona misteriosa, pero brillante y sería capaz de poder controlar la mente humana en beneficio propio.',
+        title: 'Soy como Julio de La Hermandad y tu ¿conoces cómo es tu personalidad?',
+        picture: 'https://lahermandad.github.io/pic/q1_julio.jpg',
+    }, function(response) {
+        if (response && !response.error_message) {
+            $('#lb_msj').text('link posteado' + response.post_id);
+        } else {
+            $('#lb_msj').text('error ...');
+        }
+    });
+}
+
+function shareLuisa() {
+    FB.ui({
+        method: 'share',
+        href: 'https://lahermandad.github.io/index_rol.html',
+        description: 'Soy una persona valiente, leal y noble. Sería capaz de poder combatir la corrupción en altas esferas de la política.',
+        title: 'Soy como Luisa de La Hermandad y tu ¿conoces cómo es tu personalidad?',
+        picture: 'https://lahermandad.github.io/pic/q1_luisa.jpg',
+    }, function(response) {
+        if (response && !response.error_message) {
+            $('#lb_msj').text('link posteado' + response.post_id);
+        } else {
+            $('#lb_msj').text('error ...');
+        }
+    });
+}
+
+function shareMilena() {
+    FB.ui({
+        method: 'share',
+        href: 'https://lahermandad.github.io/index_rol.html',
+        description: 'Soy una persona soñadora y fantaseo con el mundo ideal; sería capaz de cautivar a cualquiera, porque se el impacto que provoco en los demás.',
+        title: 'Soy como Milena de La Hermandad y tu ¿conoces cómo es tu personalidad?',
+        picture: 'https://lahermandad.github.io/pic/q1_milena.jpg',
+    }, function(response) {
+        if (response && !response.error_message) {
+            $('#lb_msj').text('link posteado' + response.post_id);
+        } else {
+            $('#lb_msj').text('error ...');
+        }
+    });
 }
